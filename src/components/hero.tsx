4618815d25d1dd3ee8a357d845b020e898079f4e
Hamza <hamzaelbadaoui000@gmail.com>
2026-03-14 17:28:02 +0000

    some stuff

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     <section id="hero" className="relative h-screen min-h-[700px] flex items-center justify-center text-center text-white">
       {heroImage && (
         <Image
-          src={heroImage.imageUrl}
+          src='/balcony.jpeg'
           alt={heroImage.description}
           fill
           priority
